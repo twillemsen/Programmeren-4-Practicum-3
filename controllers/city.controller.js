172dc255ea8ac3f6f4bef78e179dbc6bc8c3f931
Tobias Willemsen <twillems4@avans.nl>
2018-05-13 18:22:29 +0200

    Added getCityById, createCity and updateCityById.

--- controllers/city.controller.js
+++ controllers/city.controller.js
@@ -12,5 +12,63 @@ module.exports = {
                 result: rows
             }).end();
         });
+    },
+
+    getCityByID(req, res, next) {
+        console.log('getCityByID called');
+
+        const ID = req.params.ID;
+
+        db.query('SELECT * FROM city WHERE ID=' + ID, (error, rows, fields) => {
+            res.status(200).json({
+                status: {
+                    query: 'OK'
+                },
+                result: rows
+            }).end();
+        });
+    },
+
+    createCity(req, res, next) {
+        console.log('createCity called');
+
+        let city = req.body;
+
+        let query = {
+            sql: 'INSERT INTO city(ID, Name, CountryCode, District, Population) VALUES (?, ?, ?, ?, ?)',
+            values: [ city.ID, city.Name, city.CountryCode, city.District, city.Population ],
+            timeout: 2000
+        }
+
+        db.query(query, (error, rows, fields) => {
+            res.status(200).json({
+                status: {
+                    query: 'OK'
+                },
+                result: rows
+            }).end();
+        });
+    },
+
+    updateCityByID(req, res, next) {
+        console.log('updateCityById called');
+
+        let city = req.body;
+        let city_ID = req.params.ID;
+
+        let query = {
+            sql: 'UPDATE city SET Name=? , CountryCode=? , District=?, Population=? WHERE ID=?',
+            values: [ city.Name, city.CountryCode, city.District, city.Population, city_ID ],
+            timeout: 2000
+        }
+
+        db.query(query, (error, rows, fields) => {
+            res.status(200).json({
+                status: {
+                    query: 'OK'
+                },
+                result: rows
+            }).end();
+        });
     }
 }
--- routes/city.routes.js
+++ routes/city.routes.js
@@ -2,5 +2,8 @@ let routes = require('express').Router();
 let citycontroller = require('../controllers/city.controller');
 
 routes.get('/cities', citycontroller.getAllCities);
+routes.get('/cities/:ID', citycontroller.getCityByID);
+routes.post('/cities', citycontroller.createCity);
+routes.put('/cities/:ID', citycontroller.updateCityByID);
 
 module.exports = routes;
